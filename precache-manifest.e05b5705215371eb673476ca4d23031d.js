eabdaab0821ce7a3bcce5c4e76db4785c6a028da
Dmytro Osin <mr.oda.ua@gmail.com>
2020-09-20 00:46:44 +0300

    Updates

---
+++ precache-manifest.e05b5705215371eb673476ca4d23031d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dffb232c21003d872897c4b143fbb5",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "d6f26d780bd5b423a235",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.424b5a06.chunk.css"
+  },
+  {
+    "revision": "9553da8fd024e307c5ca",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e7da39e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.e7da39e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f26d780bd5b423a235",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.2745cbcd.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
